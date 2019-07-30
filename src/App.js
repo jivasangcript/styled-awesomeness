622c72c7a1a421586841cbc6a5325d239626aecf
jivasangcript <wona23@gmail.com>
2019-07-30 12:36:46 +0900

    #3 createGlobalStyle and Extend

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from "react";
-import styled from "styled-components";
+import styled, { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    padding: 0;
+    margin: 0;
+  }
+`;
 
 class App extends Component {
   render() {
     return (
       <Container>
+        <GlobalStyle />
         <Button>Hello</Button>
         <Button danger>Hello</Button>
+        <Anchor href="http://google.com">Go to google</Anchor>
       </Container>
     );
   }
@@ -30,6 +39,10 @@ const Button = styled.button`
     outline: none;
   }
   background-color: ${props => (props.danger ? "#e74c3c" : "#2ecc71")};
+`;
+
+const Anchor = styled(Button.withComponent("a"))`
+  text-decoration: none;
 `;
 
 export default App;
